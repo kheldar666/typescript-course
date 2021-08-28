0b53cbcc0348945274eaf9f10951907530b65530
Martin Papy <martin.papy@gmail.com>
2021-08-28 09:54:10 +0700

    Interfaces, Abtract Classes, Inheritence

---
+++ 04-sort/src/LinkedList.ts
@@ -0,0 +1,81 @@
+import { AbstractSorter } from "./Sorter";
+
+class Node {
+  next: Node | null = null;
+  constructor(public data: number) {}
+}
+
+export class LinkedList extends AbstractSorter {
+  private head: Node | null = null;
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head || index > this.length) {
+      throw new Error("index out of bound");
+    }
+    let node: Node | null = this.head;
+    let counter = 0;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    throw new Error("index out of bound");
+  }
+
+  add(data: number): void {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    tail.next = node;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    return leftNode.data > rightNode.data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+
+  print(): void {
+    if (!this.head) {
+      console.log("Empty List");
+      return;
+    }
+
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
--- 04-sort/src/index.ts
+++ 04-sort/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hi there");
+import { NumbersCollection } from "./NumbersCollection";
+import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
+
+const numbers = new NumbersCollection([10, 5, -5, 0]);
+const chars = new CharacterCollection("dùvlbqnrz *,oVKNQDÙovb^^' vùc ùfQONFn");
+const linkedList = new LinkedList();
+linkedList.add(5);
+linkedList.add(-10);
+linkedList.add(75);
+linkedList.add(-3);
+
+numbers.sort();
+console.log(numbers.data);
+
+chars.sort();
+console.log(chars.data);
+
+linkedList.sort();
+linkedList.print();
