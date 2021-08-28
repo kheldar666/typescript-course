dfc678d12c01d3bccc04dd73429972646e121069
Martin Papy <martin.papy@gmail.com>
2021-08-28 17:13:28 +0700

    Using a static method to shorten the call in index.ts

--- 05-stats/src/index.ts
+++ 05-stats/src/index.ts
@@ -7,17 +7,13 @@ import { CsvFileReader } from "./composition/CsvFileReader";
 import { MatchReader } from "./composition/MatchReader";
 import { Summary } from "./inheritance/Summary";
 import { WinsAnalysis } from "./inheritance/analyzer/WinsAnalysis";
-import { ConsoleReport } from "./inheritance/outputs/ConsoleReport";
 import { HtmlReport } from "./inheritance/outputs/HtmlReport";
 
 const matchReader = new MatchReader(new CsvFileReader("./football.csv"));
 matchReader.load();
 const matches = matchReader.matches;
 
-const consoleSummary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
+const consoleSummary = Summary.winsAnalysisAndConsoleReport("Man United"); //Static method used
 
 consoleSummary.buildAndPrintReport(matches);
 
--- 05-stats/src/inheritance/Summary.ts
+++ 05-stats/src/inheritance/Summary.ts
@@ -1,6 +1,8 @@
 import { Analyzer } from "./analyzer/Analyzer";
 import { OutputTarget } from "./outputs/OutputTarget";
 import { MatchTuple } from "../types/MatchTuple";
+import { WinsAnalysis } from "./analyzer/WinsAnalysis";
+import { ConsoleReport } from "./outputs/ConsoleReport";
 
 export class Summary {
   constructor(private analyzer: Analyzer, private outputTarget: OutputTarget) {}
@@ -8,5 +10,9 @@ export class Summary {
   buildAndPrintReport(matches: MatchTuple[]) {
     const report = this.analyzer.run(matches);
     this.outputTarget.print(report);
+  }
+
+  static winsAnalysisAndConsoleReport(team: string): Summary {
+    return new Summary(new WinsAnalysis(team), new ConsoleReport());
   }
 }
