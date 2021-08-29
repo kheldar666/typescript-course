870477fe7b59c7676450bed532c3604c988e4018
Martin Papy <martin.papy@gmail.com>
2021-08-29 09:30:36 +0700

    Continue setup Add json server

--- 06-web/src/index.ts
+++ 06-web/src/index.ts
@@ -2,7 +2,18 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Martin", age: 46 });
 
-user.set({ name: "Martin Papy" });
+user.on("change", () => {
+  console.log("Event #1 triggered");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.on("change", () => {
+  console.log("Event #2 triggered");
+});
+
+user.on("save", () => {
+  console.log("Save Event #1 triggered");
+});
+
+user.trigger("change");
+user.trigger("save");
+console.log(user);
--- 06-web/src/models/User.ts
+++ 06-web/src/models/User.ts
@@ -1,16 +1,33 @@
-export interface UserProps {
+interface UserProps {
   name?: string;
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  private events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
     return this.data[propName as keyof UserProps];
   }
 
-  set(update: UserProps) {
+  set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName] || [];
+    for (let i = 0; i < handlers.length; i++) {
+      handlers[i]();
+    }
   }
 }
