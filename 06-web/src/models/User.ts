0a60661576acb3656f6304b9111747e238c542ff
Martin Papy <martin.papy@gmail.com>
2021-08-29 10:13:21 +0700

    Adding Save/Update

--- 06-web/src/models/User.ts
+++ 06-web/src/models/User.ts
@@ -1,4 +1,9 @@
-interface UserProps {
+import axios, { AxiosResponse } from "axios";
+
+const JSON_SERVER_URL = "http://localhost:3000/users";
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -28,6 +33,32 @@ export class User {
     const handlers = this.events[eventName] || [];
     for (let i = 0; i < handlers.length; i++) {
       handlers[i]();
+    }
+  }
+
+  fetch(): void {
+    axios
+      .get(JSON_SERVER_URL + "/" + this.get("id"))
+      .then((response: AxiosResponse) => {
+        this.set(response.data as UserProps);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  save(): void {
+    const userId = this.get("id");
+    if (!userId) {
+      axios
+        .post(JSON_SERVER_URL, this.data)
+        .then((response: AxiosResponse) => console.log("User saved"))
+        .catch((err) => console.error(err));
+    } else {
+      axios
+        .put(JSON_SERVER_URL + "/" + userId, this.data)
+        .then((response: AxiosResponse) => console.log("User updated"))
+        .catch((err) => console.error(err));
     }
   }
 }
