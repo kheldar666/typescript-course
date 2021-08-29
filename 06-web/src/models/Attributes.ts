37443e26e607eca239985520e061a74c1875c0c5
Martin Papy <martin.papy@gmail.com>
2021-08-29 14:53:43 +0700

    Must use Arrow function to get the right behaviou for "this"

--- 06-web/src/models/Attributes.ts
+++ 06-web/src/models/Attributes.ts
@@ -6,19 +6,19 @@ export interface GetSetAttributes<T> {
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     const value = this.data[key];
     if (value) {
       return value;
     }
     throw new Error(`Property "${key}" undefined`);
-  }
+  };
 
-  getData(): T {
+  getData = (): T => {
     return this.data;
-  }
+  };
 
-  set(update: T): void {
+  set = (update: T): void => {
     Object.assign(this.data, update);
-  }
+  };
 }
